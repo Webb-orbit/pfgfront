30cc1afce9fe64252a5ef97f082f80594219eb00
webb-orbit <webbenemies@gmail.com>
2024-09-24 21:31:33 +0530

    update vite condig

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server:{
     proxy:{
-      "/api":"https://webb-orbit.github.io/AIPDF-"
+      "/api":"http://localhost:3000"
     }
   },
   plugins: [react()],
